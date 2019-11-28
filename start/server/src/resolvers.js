7772ddce3efba8f46983b99d0effd2913fae3b6c
Warren R Bank <warren.r.bank@gmail.com>
2019-11-28 08:10:20 -0800

    server/v04

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -37,6 +37,61 @@ module.exports = {
         ? mission.missionPatchSmall
         : mission.missionPatchLarge;
     }
+  },
+
+  Launch: {
+    isBooked: async (launch, _, { dataSources })             => dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
+  },
+
+  User: {
+    trips: async (_, __, { dataSources })                    => {
+      // get ids of launches by user
+      const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+      if (!launchIds.length) return [];
+
+      // look up those launches by their ids
+      const launches = await dataSources.launchAPI.getLaunchesByIds({ launchIds });
+      return launches || [];
+    }
+  },
+
+  Mutation: {
+    login: async (_, { email }, { dataSources })             => {
+      const user = await dataSources.userAPI.findOrCreateUser({ email });
+      if (user) return Buffer.from(email).toString('base64');
+    },
+
+    bookTrips: async (_, { launchIds }, { dataSources }) => {
+      const results  = await dataSources.userAPI.bookTrips({ launchIds });
+      const launches = await dataSources.launchAPI.getLaunchesByIds({ launchIds });
+
+      return {
+        success: results && (results.length === launchIds.length),
+        message: 
+          (results.length === launchIds.length)
+            ? 'trips booked successfully'
+            : `the following launches couldn't be booked: ${launchIds.filter( id => !results.includes(id) )}`,
+        launches
+      };
+    },
+
+    cancelTrip: async (_, { launchId }, { dataSources }) => {
+      const result = await dataSources.userAPI.cancelTrip({ launchId });
+
+      if (!result)
+        return {
+          success: false,
+          message: 'failed to cancel trip',
+        };
+
+      const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+      return {
+        success: true,
+        message: 'trip cancelled',
+        launches: [launch],
+      };
+    }
+
   }
 
 };
