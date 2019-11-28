4c07fbcde8d0e1045053361132266ea1318aad6d
Warren R Bank <warren.r.bank@gmail.com>
2019-11-28 15:28:33 -0800

    client/v06

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -10,9 +10,21 @@ import React              from 'react';
 import ReactDOM           from 'react-dom';
 import Pages              from './pages';
 
+const token = localStorage.getItem('token');
+
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: 'http://localhost:4000/'
+  uri: 'http://localhost:4000/',
+  headers: {
+    authorization: token ? token : ''
+  }
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!token,
+    cartItems: [],
+  },
 });
 
 const client = new ApolloClient({
