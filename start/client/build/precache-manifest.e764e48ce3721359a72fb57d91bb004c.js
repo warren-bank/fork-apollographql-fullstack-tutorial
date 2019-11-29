0504ecc650b544484dbd53cbce90de98012284cd
Warren R Bank <warren.r.bank@gmail.com>
2019-11-28 21:28:25 -0800

    client/v10-build

---
+++ start/client/build/precache-manifest.e764e48ce3721359a72fb57d91bb004c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2661c2f12c3c1ffce1902de1a55688d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7393ee21cf8da9f29dfe",
+    "url": "/static/js/2.0a6d9908.chunk.js"
+  },
+  {
+    "revision": "c7832aaf4b45093a48ac",
+    "url": "/static/js/main.5ca2b041.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0a4ee70bfcddd57e81c1c5280aa73122",
+    "url": "/static/media/cart.0a4ee70b.svg"
+  },
+  {
+    "revision": "33d00d4051b4139369bdfce6c6ad4ebb",
+    "url": "/static/media/curve.33d00d40.svg"
+  },
+  {
+    "revision": "f25b4cba48a496093085f268698a4b39",
+    "url": "/static/media/dog-1.f25b4cba.png"
+  },
+  {
+    "revision": "ab7a533bb445ea0d167a34cdc0eec387",
+    "url": "/static/media/dog-2.ab7a533b.png"
+  },
+  {
+    "revision": "b62803749fd80890a425b0dfae080c47",
+    "url": "/static/media/dog-3.b6280374.png"
+  },
+  {
+    "revision": "5bb388b5d1c87d4b12dff9a89ab75766",
+    "url": "/static/media/exit.5bb388b5.svg"
+  },
+  {
+    "revision": "0b1c67758d564d5eb140740847e6886e",
+    "url": "/static/media/galaxy.0b1c6775.jpg"
+  },
+  {
+    "revision": "a8474d8eef7ab919939d844420c89b6e",
+    "url": "/static/media/home.a8474d8e.svg"
+  },
+  {
+    "revision": "48932214f494cf346794f464ee2c04e3",
+    "url": "/static/media/iss.48932214.jpg"
+  },
+  {
+    "revision": "3e5f8e367babc8286909705d3f93c10d",
+    "url": "/static/media/logo.3e5f8e36.svg"
+  },
+  {
+    "revision": "493bbb752333aacb0e80bab31c2e925a",
+    "url": "/static/media/moon.493bbb75.jpg"
+  },
+  {
+    "revision": "3eddd92283b12f509d52858bbf7fbf60",
+    "url": "/static/media/profile.3eddd922.svg"
+  },
+  {
+    "revision": "aeabe03413a40651e399da5c465937eb",
+    "url": "/static/media/rocket.aeabe034.svg"
+  },
+  {
+    "revision": "7dfc79232cbb090253471fe744fe5680",
+    "url": "/static/media/space.7dfc7923.jpg"
+  }
+]);
